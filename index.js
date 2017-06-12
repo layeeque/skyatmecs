65533594958e9c263c6760bf115bca6ba921ee60
layeeque <layeequerehman786@gmail.com>
2017-06-12 12:46:19 +0530

    Create index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var https = require('https');
 const restService = express();
+var data1="";
 
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -12,13 +11,98 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
+
+    
+     var c = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+
+     var obj="";
+  //  //var api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAbMVp_Kmi6Ixrh6RfVLAbW_mMAY0O7itQ&cx=013351406654656600973:5gj2eij-z1i&q=encodeURIComponent("+c+")";
+    var api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAbMVp_Kmi6Ixrh6RfVLAbW_mMAY0O7itQ&cx=013351406654656600973:5gj2eij-z1i&q="+c;
+    
+  
+         https.get(api, (response) => {
+             var body = "";
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+               console.log("===================================================================================");   
+                 //var bodya=body.items
+                  console.log("body: "+body);
+            //  var bodya = body.searchInformation.totalResults;
+                  //return res.json(bodya);
+              obj =JSON.parse(body);
+               // var obj1=JSON.parse(obj).title;
+             
+                
+                  //console.log(body
+//.displayText.items[0].snipppet
+//)
+                // console.log("obj1 : "+obj1)
+                return res.json({
+            speech: obj.items[0].snippet,
+             displayText: obj.items[0].snippet,
+          source: 'webhook-echo-sample'
+      });
+   // return res.json(obj.items[0].snippet);
+      //return res.json(obj);
+              })
+            })
     });
-});
+
+   // var q = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+  
+  //var api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDcpIw1u0qoDICOZsYMiKrxuLCHUstzIT4&cx=000792015022037580768:80cf2nj_1g0&q=encodeURIComponent(salman)";
+//var response="";
+//var content="";
+
+// var data1=""
+//   var body = "";
+//             https.get(api, (response) => {
+//               response.on('data', (chunk) => { body += chunk })
+//               response.on('end', () => {
+//                 data = JSON.parse(body);
+//                 console.log("Data : "+data);
+//                 data1=data;
+//               })
+//             })
+
+//console.log(body);
+//console.log(data);
+  //try {
+
+    // response = UrlFetchApp.fetch(api, {
+    //  muteHttpExceptions: true
+   // });
+     // content = JSON.parse(response);
+    //if (response.getResponseCode() == 200) {
+
+     // var content = JSON.parse(response);
+ 
+    //   // Did the search return any results?
+    //   if (content.searchInformation.totalResults > 0) {
+
+    //     var count = content.items.length;
+
+    //     for (var i = 0; i < count; i++) {
+
+    //       // Save the page title, description and hyperlink
+    //       Logger.log(content.items[i].title);
+    //       Logger.log(content.items[i].snippet);
+    //       Logger.log(content.items[i].link);
+    //     }
+    //   }
+   //}
+ // } catch (f) {
+   //Logger.log(f.toString());
+  //}
+  
+  
+  
+    // return res.json({
+    //     speech: "danish".concat(data1),
+    //     displayText: "danish".concat(data1),
+    //     source: 'webhook-echo-sample'
+    // });
+//});
 
 restService.post('/slack-test', function(req, res) {
 
